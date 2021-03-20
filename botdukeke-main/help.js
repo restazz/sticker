20562b3473e8f340ac766a56e92e9af8242ffa15
restazz <rest9z@outlook.com>
2021-03-19 23:16:19 -0300

    first commit

---
+++ botdukeke-main/help.js
@@ -0,0 +1,98 @@
+
+const help = (prefix) => { 
+	return `                 
+<═══════════════════════>
+                乃ㄖㄒ ᗪㄖ 尺乇丂ㄒ卂乙
+ <═══════════════════════>
+
+             ╲    ╱    ● ᏴϴͲ●ᎷᎬΝႮ●
+       ╱▔▔▔▔▔╲  
+      ┃┈▇┈┈▇┈┃      
+╭╮┣━━━━━━┫╭╮    
+┃┃┃┈┈┈┈┈┈┃┃┃    
+╰╯┃┈┈┈┈┈┈┃╰╯
+      ╰┓┏━━┓┏╯
+         ╰╯      ╰╯
+
+ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ 
+ ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+╔═════════════════════════╗
+ ●─────────●MENU●──────────●
+╚═════════════════════════╝
+
+╔══════════════════════
+║ ᏟϴᎷᎪΝᎠϴՏ ᏞᏆՏͲᎪᎠϴՏ
+╠══════════════════════
+║   ᎬХᏟᏞႮՏᏆᏙϴ ᏢᎪᎡᎪ ᎪᎠᎷ'Տ
+║╭─────────────────────
+║├────> ${prefix}kick @menciona o membro
+║├> banir membro 
+║├─────────────────────
+║├> ${prefix}add +55xxxxxxxxx
+║├────> add menbro
+║├─────────────────────
+║├> ${prefix}listadmin
+║├────> listar cm todos ADM's
+║├─────────────────────
+║├> ${prefix}welcome [1/0]
+║├────> ativar/desativar boas vindas
+║├─────────────────────
+║├> ${prefix}group tutup
+║├────> fecha grupo 
+║├─────────────────────
+║├> ${prefix}group buka
+║├────> abrir grupo 
+║├─────────────────────
+║├> ${prefix}demote @marca adm
+║├────> retirar ADM de alguém
+║├─────────────────────
+║├> ${prefix}promote @marca membro
+║├────> promover a ADM
+║├─────────────────────
+╠══════════════════════
+║   ᎷᏆᎠᏆᎪ Ꮖ ՏͲᏆᏟᏦᎬᎡ
+║╭─────────────────────
+║├> ${prefix}sticker + foto/Gif
+║├────> fazer figurinha
+║├─────────────────────
+║├> ${prefix}anjing
+║├────> dog aleatório fofinho
+║├─────────────────────
+║├> ${prefix}text3d seu txt
+║├────> uma foto cm seu txt
+║├─────────────────────
+║├> ${prefix}pokemon
+║├────> um pokemon aleatório
+║├─────────────────────
+║├> ${prefix}ocr (marque uma foto)
+║├────> pegar txts de uma foto
+║├─────────────────────
+║├> ${prefix}tts pt seu txt
+║├────> transformar txt em áudio
+║├─────────────────────
+║├> ${prefix}toimg (marca uma fig)
+║├────> converter fig em foto
+║├─────────────────────
+║├> ${prefix}ytmp4 (link de algum vídeo)
+║├────> pega informações de vídeo
+║├─────────────────────
+║├> ${prefix}map 00000-000
+║├────> pega imagem do local
+║│desejado 
+║├─────────────────────
+╠══════════════════════
+║   ᏟϴᎷᎪΝᎠϴՏ ᏢᎪᎡᎪ ϴ ᏟᎡᏆᎪᎠϴᎡ
+║╭─────────────────────
+║├> ${prefix}clearall
+║├────> sla
+║├─────────────────────
+
+
+`
+}
+exports.help = help
+
+
+
+  
